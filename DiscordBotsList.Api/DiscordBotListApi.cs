968ae7a08fb82fd5ab64ae4bedfb98ed4c462331
Xwilarg <xwilarg@yahoo.fr>
2019-06-12 11:34:48 +0200

    Remove Miki.Net.Http

--- DiscordBotsList.Api/DiscordBotListApi.cs
+++ DiscordBotsList.Api/DiscordBotListApi.cs
@@ -1,10 +1,9 @@
 ﻿using DiscordBotsList.Api.Internal;
 using DiscordBotsList.Api.Internal.Queries;
-using Miki.Rest;
 using System.Threading.Tasks;
 using DiscordBotsList.Api.Objects;
+using System.Net.Http;
 using Newtonsoft.Json;
-using Miki.Net.Http;
 
 namespace DiscordBotsList.Api
 {
@@ -14,7 +13,7 @@ namespace DiscordBotsList.Api
 
 		public DiscordBotListApi()
 		{
-			_httpClient = new HttpClient("https://discordbots.org/api/");
+            _httpClient = new HttpClient();
 		}
 
 		/// <summary>
@@ -80,12 +79,10 @@ namespace DiscordBotsList.Api
 		/// <returns>Object of type T</returns>
 		protected async Task<T> GetAsync<T>(string url)
 		{
-			HttpResponse<T> t = await _httpClient.GetAsync<T>(url);
-			if (t.Success)
-			{
-				return t.Data;
-			}
-			return default(T);
+			HttpResponseMessage t = await _httpClient.GetAsync("https://discordbots.org/api/" + url);
+            if (t.IsSuccessStatusCode)
+				return JsonConvert.DeserializeObject<T>(await t.Content.ReadAsStringAsync());
+			return default;
 		}
 
 	    /// <summary>
@@ -93,6 +90,6 @@ namespace DiscordBotsList.Api
 	    /// </summary>
 	    /// <returns>True or False</returns>
 	    public async Task<bool> IsWeekendAsync()
-			=> (await _httpClient.GetAsync<WeekendObject>("weekend")).Data.Weekend;
+			=> (await GetAsync<WeekendObject>("weekend")).Weekend;
 	}
 }
