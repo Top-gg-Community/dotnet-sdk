c7912233f80284df5eba8da6791c986b904eb729
Abner <25711694+AbnerSquared@users.noreply.github.com>
2019-10-12 15:42:56 -0500

    Create DblField, used when searching for bots
    This enumeration contains all of the possible fields that can be used as an argument when searching for bots.

---
+++ DiscordBotsList.Api/DblField.cs
@@ -0,0 +1,29 @@
+namespace DiscordBotList.Api
+{
+    // Complete.
+    public enum DblField
+    {
+        Id = 1,
+        Username,
+        Discriminator,
+        Avatar,
+        DefAvatar,
+        Lib,
+        Prefix,
+        ShortDesc,
+        LongDesc,
+        Tags,
+        Website,
+        Support,
+        Github,
+        Owners,
+        Guilds,
+        Invite,
+        Date,
+        CertifiedBot,
+        Vanity,
+        Points,
+        MonthlyPoints,
+        DonateBotGuildId
+    }
+}
